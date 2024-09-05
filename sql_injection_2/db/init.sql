d79f91305ba76b2d5234d32d5f0a138504294933
JuanAriza17 <60227446+JuanAriza17@users.noreply.github.com>
2024-09-05 01:08:09 -0500

    xd

--- sql_injection_2/db/init.sql
+++ sql_injection_2/db/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    pswd VALUES(50) NOT NULL
+    password VARCHAR(50) NOT NULL
 );
 
-INSERT INTO users (username, pswd) VALUES ('admin', 'password123'), ('user', 'asuioldfhewdfajkeo'), ('test', 'adjkasdfghserfefsdf');
+INSERT INTO users (username, password) VALUES ('admin', 'password123'), ('user', 'asuioldfhewdfajkeo'), ('test', 'adjkasdfghserfefsdf');
